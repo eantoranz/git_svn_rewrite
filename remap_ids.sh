95d3cbe10d657978537da7d1035e6c56bfbaf922
Edmundo Carmona Antoranz <eantoranz@gmail.com>
2015-12-27 21:13:37 -0600

    Use file to hold revision mapping (passed along as an environment variable)

--- remap_ids.sh
+++ remap_ids.sh
@@ -7,7 +7,7 @@
 # git filter-branch --commit-filter '/path/to/script/remap_ids.sh "$@"'
 
 export GIT_AUTHOR_NAME="Test User"
-NEW_REV=$( git commit-tree "$@" );
-echo revision $GIT_COMMIT $NEW_REV >&2;
+NEW_REV=$( git commit-tree "$@" )
+echo $GIT_COMMIT $NEW_REV >> $SVN_REVISION_FILE
 echo $NEW_REV
 
