6a572f000ead3f5e7e217c26fa3b4227aea4dbb9
Edmundo Carmona Antoranz <eantoranz@gmail.com>
2017-06-07 19:09:13 -0600

    Have a script to write the 'new' merge commit that will replace the merge segment

--- blend-git/blend-git.sh
+++ blend-git/blend-git.sh
@@ -8,30 +8,12 @@
 # REV2: last revision to REMOVE from history
 # REV3: revision that will be used to replace REV2
 
-# get the full ID of a revision
-function get_full_revision_id {
-    res=$( git show --summary -q --pretty=%H $1 )
-    if [ $? -ne 0 ]; then
-        echo Error getting full revision ID for $1 >&2
-        exit 1
-    fi
-    echo $res
-}
-
-# Get the tree object ID of a revision
-function get_tree_object_id {
-    tree=$( git cat-file -p $1 | grep tree | head -n 1 | sed 's/^tree*. //' )
-    if [ $? -ne 0 ]; then
-        echo Error getting the tree object ID for revision $1 >&2
-        exit 1
-    fi
-    echo $tree
-}
-
 if [ $# -lt 4 ]; then
     echo Not enough paremeters
     exit 1
 fi
+
+source $( dirname ${BASH_SOURCE[0]} )/common.sh
 
 TEMP_FILE="$1" # temporary file to save the mapping between revisions
 
